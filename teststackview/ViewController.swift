b7bfed9f81e44838741ee0568fb596fc39d2fb68
Abderrazak Berrah <abderrazakberrah@Abderrazaks-MacBook-Pro.local>
2019-01-30 11:38:52 +0100

    Initial Commit

---
+++ teststackview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teststackview
+//
+//  Created by Abderrazak Berrah on 30/01/2019.
+//  Copyright © 2019 Abderrazak Berrah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
